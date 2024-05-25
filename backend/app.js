f1cbd0c7ee43ede9555315a700cb6f18a141cce4
Marcel <marcel@Marcels-MacBook-Air.local>
2024-05-25 19:47:13 +0200

    initial setup

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const threatRoutes = require('./routes/threatRoutes');
+
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/threats', threatRoutes);
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running successfully');
+});
+
+module.exports = app;
