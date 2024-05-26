0da2a45c0d30224a042f0774a0f315f26c1f5dac
marcelmroz <mmroz.marcel@gmail.com>
2024-05-26 14:35:58 +0200

    connected to db

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const threatRoutes = require('./routes/threatRoutes');
+const db = require('./config/db');
 
-app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/threats', threatRoutes);
+const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Server is running successfully');
+});
+
+app.get('/test-db', (req, res) => {
+  db.query('SELECT 1 + 1 AS solution', (err, results) => {
+    if (err) throw err;
+    res.send(`Database connected: ${results[0].solution}`);
+  });
 });
 
 module.exports = app;
